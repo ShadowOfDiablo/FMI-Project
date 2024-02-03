e7309cd1192d07ea15db17e94162776c5ced094a
Tihomir Dimitrov <114056948+ShadowOfDiablo@users.noreply.github.com>
2024-02-03 11:13:16 +0200

    Update FrontendEndpoint.java

--- LibarterBE-main/libarter-be/src/main/java/com/bryan/libarterbe/configuration/FrontendEndpoint.java
+++ LibarterBE-main/libarter-be/src/main/java/com/bryan/libarterbe/configuration/FrontendEndpoint.java
@@ -1,5 +1,5 @@
 package com.bryan.libarterbe.configuration;
 
 public class FrontendEndpoint {
-    public static String endpoint = "libarterfe.azurewebsites.net";
+    public static String endpoint = "yellow-pond-09924c203.4.azurestaticapps.net";
 }
